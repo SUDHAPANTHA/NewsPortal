747aa918260a4bcfd992faec3041185697ad7235
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-21 22:00:23 +0545

    My commit message

---
+++ news_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from news_app.models import Post
+
+
+class HomePageView(ListView): 
+    model = Post
+    template_name = 'aznews/home.html'
+    context_object_name = "posts"
+    queryset = Post.objects.filter(
+        published_at__isnull=False, status="active"
+    ).order_by("-published_at")[:5]
