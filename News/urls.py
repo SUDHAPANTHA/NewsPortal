8abf667c9bf23b0fe91be50b8faef620cba7e421
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-22 21:58:58 +0545

    image

--- News/urls.py
+++ News/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from news_app import views
 
@@ -23,3 +25,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("news_app.urls")),
 ]
+if settings.DEBUG:  # Only serve media files in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
