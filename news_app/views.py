68cb3cc5d34366f7df970177fb543d57bef09d67
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-22 20:57:29 +0545

    changes on urls

--- news_app/views.py
+++ news_app/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+
 from news_app.models import Post
 
-
-class HomePageView(ListView): 
+class HomePageView(ListView):
     model = Post
-    template_name = 'aznews/home.html'
+    template_name = "aznews/home.html"
     context_object_name = "posts"
-    queryset = Post.objects.filter(
-        published_at__isnull=False, status="active"
-    ).order_by("-published_at")[:5]
+    # queryset = Post.objects.filter(
+    #     published_at_isnull=False, status="active"
+    # ).order_by("-publised_at")[:5]
+
