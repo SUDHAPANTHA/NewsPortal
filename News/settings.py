1f26e06b3dc1fae60e60697fbbac6e7307aab0c1
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-26 09:58:27 +0545

    images

--- News/settings.py
+++ News/settings.py
@@ -74,10 +74,20 @@ WSGI_APPLICATION = 'News.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'news_portal',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -120,7 +130,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = ["static"]
 
 MEDIA_URL = "/media/"  
-MEDIA_ROOT = "media"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 # Default primary key field type
