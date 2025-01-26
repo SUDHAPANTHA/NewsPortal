096e42ed57b04e3d35a6baf4d7f1ef0b8553fa08
SUDHAPANTHA <sudhapantha111@gmail.com>
2025-01-26 10:26:07 +0545

    trending_right

--- news_app/views.py
+++ news_app/views.py
@@ -7,7 +7,33 @@ class HomePageView(ListView):
     model = Post
     template_name = "aznews/home.html"
     context_object_name = "posts"
-    # queryset = Post.objects.filter(
-    #     published_at_isnull=False, status="active"
-    # ).order_by("-publised_at")[:5]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["trending_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-views_count")[:3]
+
+        context["featured_post"] = (
+            Post.objects.filter(published_at__isnull=False, status="active")
+            .order_by("-published_at", "-views_count")
+            .first()
+        )
+        context["featured_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at", "-views_count")[1:4]
+
+        from datetime import timedelta
+        from django.utils import timezone
+
+        one_week_ago = timezone.now() - timedelta(days=7)
+        context["weekly_top_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active", published_at__gte=one_week_ago
+        ).order_by("-published_at", "-views_count")[:7]
+
+        context["recent_posts"] = Post.objects.filter(
+            published_at__isnull=False, status="active"
+        ).order_by("-published_at")[:7]
+
+        return context
 
